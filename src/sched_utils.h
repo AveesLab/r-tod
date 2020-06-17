83424269428edadf03ddfad9b3d5b8d41e1ab8ec
wonseok-Jang <wsjang9017@gmail.com>
2020-06-17 14:34:54 +0900

    Add scheduling policy

---
+++ src/sched_utils.h
@@ -0,0 +1,117 @@
+/*
+ * Libdl
+ *  (C) Dario Faggioli <raistlin@linux.it>, 2009, 2010
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License (COPYING file) for more details.
+ *
+ */
+
+#ifndef __SCHED_UTILS__
+#define __SCHED_UTILS__
+
+#define _GNU_SOURCE
+#include <linux/kernel.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <linux/types.h>
+#include <sched.h>
+#include <linux/sched.h>
+#include <sys/types.h>
+
+#define SCHED_DEADLINE	6
+
+/* __NR_sched_setattr number */
+#ifndef __NR_sched_setattr
+#ifdef __x86_64__
+#define __NR_sched_setattr		314
+#endif
+
+#ifdef __i386__
+#define __NR_sched_setattr		351
+#endif
+
+#ifdef __arm__
+#define __NR_sched_setattr		380
+#endif
+
+#ifdef __aarch64__
+#define __NR_sched_setattr		274
+#endif
+#endif
+
+/* __NR_sched_getattr number */
+#ifndef __NR_sched_getattr
+#ifdef __x86_64__
+#define __NR_sched_getattr		315
+#endif
+
+#ifdef __i386__
+#define __NR_sched_getattr		352
+#endif
+
+#ifdef __arm__
+#define __NR_sched_getattr		381
+#endif
+
+#ifdef __aarch64__
+#define __NR_sched_getattr		275
+#endif
+#endif
+
+#define SF_SIG_RORUN		2
+#define SF_SIG_DMISS		4
+#define SF_BWRECL_DL		8
+#define SF_BWRECL_RT		16
+#define SF_BWRECL_OTH		32
+
+#define RLIMIT_DLDLINE		16
+#define RLIMIT_DLRTIME		17
+
+struct sched_attr {
+	__u32 size;
+
+	__u32 sched_policy;
+	__u64 sched_flags;
+
+	/* SCHED_NORMAL, SCHED_BATCH */
+	__s32 sched_nice;
+
+	/* SCHED_FIFO, SCHED_RR */
+	__u32 sched_priority;
+
+	/* SCHED_DEADLINE */
+	__u64 sched_runtime;
+	__u64 sched_deadline;
+	__u64 sched_period;
+};
+
+int sched_setattr(pid_t pid,
+		      const struct sched_attr *attr,
+		      unsigned int flags)
+{
+	return syscall(__NR_sched_setattr, pid, attr, flags);
+}
+
+int sched_getattr(pid_t pid,
+		      struct sched_attr *attr,
+		      unsigned int size,
+		      unsigned int flags)
+{
+	return syscall(__NR_sched_getattr, pid, attr, size, flags);
+}
+
+pid_t gettid(void)
+{
+        return syscall(__NR_gettid);
+}
+
+#endif /* __SCHED_UTILS__ */
+
