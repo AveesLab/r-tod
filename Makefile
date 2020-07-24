3eeab64cc7c7de3a60cf05e6ccc62b1c7cc37ec4
wonseok-Jang <wsjang9017@gmail.com>
2020-07-24 14:39:44 +0900

    Add R-TOD

--- Makefile
+++ Makefile
@@ -7,9 +7,9 @@ OPENMP=0
 LIBSO=0
 ZED_CAMERA=0 # ZED SDK 3.0 and above
 ZED_CAMERA_v2_8=0 # ZED SDK 2.X
-V4L2=0
-TRACE=0
-ZERO_SLACK=1
+V4L2=1
+ZERO_SLACK=0
+MEASUREMENT=1
 
 # set GPU=1 and CUDNN=1 to speedup on GPU
 # set CUDNN_HALF=1 to further speedup 3 x times (Mixed-precision on Tensor Cores) GPU: Volta, Xavier, Turing and higher
@@ -70,12 +70,13 @@ LDFLAGS= -lm -pthread -lrt
 COMMON= -Iinclude/ -I3rdparty/stb/include
 CFLAGS=-Wall -Wfatal-errors -Wno-unused-result -Wno-unknown-pragmas -fPIC -Wextra -pedantic
 
-ifeq ($(ZERO_SLACK), 1)
-CFLAGS+= -DZERO_SLACK
+ifeq ($(MEASUREMENT), 1)
+CFLAGS+= -DMEASUREMENT
 endif
 
-ifeq ($(TRACE), 1)
-CFLAGS+= -DTRACE
+
+ifeq ($(ZERO_SLACK), 1)
+CFLAGS+= -DZERO_SLACK
 endif
 
 ifeq ($(V4L2), 1)
@@ -150,7 +151,7 @@ LDFLAGS+= -L/usr/local/zed/lib -lsl_zed
 endif
 endif
 
-OBJ=image_opencv.o http_stream.o gemm.o utils.o dark_cuda.o convolutional_layer.o list.o image.o activations.o im2col.o col2im.o blas.o crop_layer.o dropout_layer.o maxpool_layer.o softmax_layer.o data.o matrix.o network.o connected_layer.o cost_layer.o parser.o option_list.o darknet.o detection_layer.o captcha.o route_layer.o writing.o box.o nightmare.o normalization_layer.o avgpool_layer.o coco.o dice.o yolo.o detector.o layer.o compare.o classifier.o local_layer.o swag.o shortcut_layer.o activation_layer.o rnn_layer.o gru_layer.o rnn.o rnn_vid.o crnn_layer.o demo.o tag.o cifar.o go.o batchnorm_layer.o art.o region_layer.o reorg_layer.o reorg_old_layer.o super.o voxel.o tree.o yolo_layer.o gaussian_yolo_layer.o upsample_layer.o lstm_layer.o conv_lstm_layer.o scale_channels_layer.o sam_layer.o v4l2.o r_tod.o
+OBJ=image_opencv.o http_stream.o gemm.o utils.o dark_cuda.o convolutional_layer.o list.o image.o activations.o im2col.o col2im.o blas.o crop_layer.o dropout_layer.o maxpool_layer.o softmax_layer.o data.o matrix.o network.o connected_layer.o cost_layer.o parser.o option_list.o darknet.o detection_layer.o captcha.o route_layer.o writing.o box.o nightmare.o normalization_layer.o avgpool_layer.o coco.o dice.o yolo.o detector.o layer.o compare.o classifier.o local_layer.o swag.o shortcut_layer.o activation_layer.o rnn_layer.o gru_layer.o rnn.o rnn_vid.o crnn_layer.o demo.o tag.o cifar.o go.o batchnorm_layer.o art.o region_layer.o reorg_layer.o reorg_old_layer.o super.o voxel.o tree.o yolo_layer.o gaussian_yolo_layer.o upsample_layer.o lstm_layer.o conv_lstm_layer.o scale_channels_layer.o sam_layer.o v4l2.o rtod.o
 ifeq ($(GPU), 1) 
 LDFLAGS+= -lstdc++ 
 OBJ+=convolutional_kernels.o activation_kernels.o im2col_kernels.o col2im_kernels.o blas_kernels.o crop_layer_kernels.o dropout_layer_kernels.o maxpool_layer_kernels.o network_kernels.o avgpool_layer_kernels.o cudaExample1.o
---
+++ src/rtod.h
@@ -0,0 +1,152 @@
+#ifndef RTOD_H
+#define RTOD_H
+
+#include "image.h"
+
+#if (defined ZERO_SLACK)
+#define cycle_offset 100
+#else
+#define cycle_offset 25
+#endif
+
+#define obj_det_cycle_idx 100
+#define cycle 1
+#define QLEN 4
+#define NFRAMES 3
+
+#define MAX(x,y) (((x) < (y) ? (y) : (x)))
+#define MIN(x,y) (((x) < (y) ? (x) : (y)))
+/* calculate inter frame gap */
+#define GET_IFG(x,y) ((x) - (y)); \
+    (y) = (x);
+
+/* Measurement */
+#define MEASUREMENT_PATH "measure"
+#define MEASUREMENT_FILE "/measure.csv"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void rtod(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int cam_index, const char *filename, char **names, int classes,
+    int frame_skip, char *prefix, char *out_filename, int mjpeg_port, int json_port, int dont_show, int ext_output, int letter_box_in, int time_limit_sec, char *http_post_host, int benchmark, int benchmark_layers);
+
+#ifdef __cplusplus
+}
+#endif
+
+struct frame_data frame[3]; // v4l2 image data
+
+double e_fetch_array[obj_det_cycle_idx];
+double b_fetch_array[obj_det_cycle_idx];
+double d_fetch_array[obj_det_cycle_idx];
+double e_infer_cpu_array[obj_det_cycle_idx];
+double e_infer_gpu_array[obj_det_cycle_idx];
+double d_infer_array[obj_det_cycle_idx];
+double e_disp_array[obj_det_cycle_idx];
+double b_disp_array[obj_det_cycle_idx];
+double d_disp_array[obj_det_cycle_idx];
+double slack[obj_det_cycle_idx];
+double fps_array[obj_det_cycle_idx];
+double e2e_delay[obj_det_cycle_idx];
+int inter_frame_gap_array[obj_det_cycle_idx];
+double cycle_time_array[obj_det_cycle_idx];
+int num_object_array[obj_det_cycle_idx];
+double transfer_delay_array[obj_det_cycle_idx];
+
+double e_fetch_sum;
+double b_fetch_sum;
+double d_fetch_sum;
+double e_infer_cpu_sum;
+double e_infer_gpu_sum;
+double d_infer_sum;
+double e_disp_sum;
+double b_disp_sum;
+double d_disp_sum;
+double slack_sum;
+double e2e_delay_sum;
+double fps_sum;
+double cycle_time_sum;
+double inter_frame_gap_sum;
+double num_object_sum;
+double trace_data_sum;
+
+#ifdef ZERO_SLACK
+double s_min;
+double e_fetch_max;
+double b_fetch_max;
+#endif
+
+double frame_timestamp[3];
+int buff_index;
+int sleep_time;
+int cnt;
+int display_index;
+int detect_index;
+int fetch_offset; // zero slack
+int cycle_index;
+double cycle_array[QLEN];
+int ondemand;
+int num_object;
+
+int frame_sequence_tmp;
+int inter_frame_gap;
+
+double start_infer;
+double end_infer;
+double d_infer;
+double d_disp;
+double start_disp;
+double end_disp;
+double start_fetch;
+double end_fetch;
+double d_fetch;
+double e_fetch;
+double b_fetch;
+double image_waiting_time;
+double select_time;
+double slack_time;
+double cycle_end;
+double transfer_delay;
+double draw_bbox_time;
+double waitkey_start;
+double e_infer_gpu;
+double b_disp;
+
+char **demo_names;
+image **demo_alphabet;
+int demo_classes;
+
+int nboxes;
+detection *dets;
+
+network net;
+image in_s ;
+image det_s;
+
+cap_cv *cap;
+float fps;
+float demo_thresh;
+int demo_ext_output;
+long long int frame_id;
+int demo_json_port;
+
+float* predictions[NFRAMES];
+int demo_index;
+mat_cv* cv_images[NFRAMES];
+float *avg;
+
+mat_cv* in_img;
+mat_cv* det_img;
+mat_cv* show_img;
+
+volatile int flag_exit;
+int letter_box;
+
+int get_rand_offset(int a, int b);
+double gettime_after_boot();
+int check_on_demand();
+void *rtod_fetch_thread(void *ptr);
+void *rtod_inference_thread(void *ptr);
+void *rtod_display_thread(void *ptr);
+
+#endif
